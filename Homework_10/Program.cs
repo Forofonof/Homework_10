ba010617547d87a992805eacb6aaa2d92a35acc4
Forofonof <DanielResult1@outlook.com>
2022-04-04 14:04:46 +0300

    Добавьте файлы проекта.

---
+++ Homework_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomNumber;
+        int divisibleNumberThree;
+        int divisibleNumberFive;
+        int sumNumber;
+        while (true)
+        {
+            randomNumber = random.Next(0, 101);
+            divisibleNumberThree = randomNumber / 3;
+            divisibleNumberFive = randomNumber / 5;
+            sumNumber = divisibleNumberFive + divisibleNumberThree;
+
+            Console.WriteLine($"случайное число: {randomNumber}. Сумма чисел, которые кратные 3 и 5: {sumNumber}.");
+            Console.ReadKey();
+        }
+    }
+}
