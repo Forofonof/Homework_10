c25b4983000241ff9ba4ef5e9d49bff7ecb83738
Forofonof <DanielResult1@outlook.com>
2022-04-05 15:34:51 +0300

    FIX_1.4

--- Homework_10/Program.cs
+++ Homework_10/Program.cs
@@ -9,23 +9,19 @@
         int numberDivisibleA = 3;
         int numberDivisibleB = 5;
         int sum = 0;
-        int sumA;
-        int sumB;
         int numberMinimum = 0;
         int numberMaximum = 100;
-        int j = 0;
+        int i;
 
         number = random.Next(numberMinimum, numberMaximum);
 
-        for (; numberMinimum <= number; numberMinimum++)
+        for (i = 0; numberMinimum <= number; numberMinimum++)
         {
-            sumA = numberMinimum / numberDivisibleA;
-            sumB = numberMinimum / numberDivisibleB;
-            if ((sumA / numberDivisibleA == j) || (sumB / numberDivisibleB == j))
+            if (numberMinimum % numberDivisibleA == i || numberMinimum % numberDivisibleB == i)
             {
-                sum = sumA + sumB;
+                sum += numberMinimum;
             }
         }
-        Console.WriteLine($"случайное число: {number}. Сумма кратных чисел 3 или 5: {sum}");
+        Console.WriteLine($"случайное число: {number}. Сумма кратных чисел {numberDivisibleA} или {numberDivisibleB}: {sum}");
     }
 }
